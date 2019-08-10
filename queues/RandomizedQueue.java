c5bde8a1ceadd384dbb477c0af4d6c01da2023de
francois.stiennon <francois.stiennon@gmail.com>
2019-08-10 17:16:06 +0100

    RandomizedQueue

--- queues/RandomizedQueue.java
+++ queues/RandomizedQueue.java
@@ -1,45 +1,28 @@
 import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    private class LinkedStack {
-        private Node first = null;
-
-        private class Node {
-            int item;
-            Node next;
-        }
-
-        public boolean isEmpty() {
-            return first == null;
-        }
-
-        public void push(int item) {
-            Node oldfirst = first;
-            first = new Node();
-            first.item = item;
-            first.next = oldfirst;
-        }
-
-        public int pop() {
-            int item = first.item;
-            first = first.next;
-            return item;
-        }
-    }
-
-
-
-    private Item[] a;
+    private Item[] array;
+    private int[] shuffle;
     private int n = 0;
-    private int used = 0;
-    private LinkedStack free;
+    private int arrayLenght = 10;
 
     // construct an empty randomized queue
     public RandomizedQueue() {
-        a = (Item[]) new Object[10];
-        free = new LinkedStack();
+        array = (Item[]) new Object[arrayLenght];
+        shuffle = intArray(arrayLenght);
+        StdRandom.shuffle(shuffle);
+    }
+
+    private int[] intArray(int c) {
+        int[] r = new int[c];
+        for (int i = 0; i < c; i++) {
+            r[i] = i;
+        }
+        return r;
     }
 
     // is the randomized queue empty?
@@ -50,60 +33,85 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // add the item
     public void enqueue(Item item) {
-        if(free.isEmpty()) {
-            a[n] = item;
-            used++;
-        }
-        else {
-            int index = free.pop();
-            a[index] = item;
-        }
+        if(item == null) throw new IllegalArgumentException();
+        StdRandom.shuffle(shuffle, n, arrayLenght);
+        array[shuffle[n]] = item;
         n++;
     }
 
     // remove and return a random item
     public Item dequeue() {
-        int index = StdRandom.uniform(used);
+        if(isEmpty()) throw new NoSuchElementException();
+        int i = StdRandom.uniform(n);
+        Item toReturn = array[shuffle[i]];
 
-        while(a[index] == null) {
-            index = StdRandom.uniform(used);
-        }
+        int iIndex = shuffle[i];
+        int lastIndex = shuffle[n-1];
 
-        Item toReturn = a[index];
-        a[index] = null;
+        shuffle[i] = lastIndex;
+        shuffle[n-1] = iIndex;
 
-        free.push(index);
-
+        n--;
         return toReturn;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-        int index = StdRandom.uniform(used);
-
-        while(a[index] == null) {
-            index = StdRandom.uniform(used);
-        }
-
-        return a[index];
+        if(isEmpty()) throw new NoSuchElementException();
+        return array[shuffle[StdRandom.uniform(n)]];
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
+        StdRandom.shuffle(shuffle, 0, n);
         return new ArrayIterator();
     }
 
     private class ArrayIterator implements Iterator<Item> {
 
-        public boolean hasNext()
+        private int index = 0;
+
+        public boolean hasNext() { return index < n; }
 
         public void remove() { throw new UnsupportedOperationException(); }
 
-        public Item next()
+        public Item next() {
+            if(!hasNext()) throw new NoSuchElementException();
+            Item toReturn = array[shuffle[index]];
+            index++;
+            return toReturn;
+        }
 
     }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+
+        System.out.println(rq.isEmpty());
+
+        rq.enqueue("it");
+
+        System.out.println(rq.isEmpty());
+
+        rq.enqueue("was");
+        rq.enqueue("the");
+        rq.enqueue("best");
+        rq.enqueue("of");
+        rq.enqueue("times");
+
+        System.out.println(rq.size());
+
+        System.out.println(rq.dequeue());
+        System.out.println(rq.dequeue());
+
+        rq.enqueue("hello");
+        rq.enqueue("world");
+        rq.enqueue("Francois");
+
+        for(String str : rq) {
+            System.out.println(str);
+        }
+    }
 
 }
