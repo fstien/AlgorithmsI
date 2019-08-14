e8122a5cbcdcbb5711e4b960aadcff29f4892ea9
francois.stiennon <francois.stiennon@gmail.com>
2019-08-14 09:16:57 +0100

    brute

--- collinear/BruteCollinearPoints.java
+++ collinear/BruteCollinearPoints.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 public class BruteCollinearPoints {
 
@@ -19,7 +22,13 @@ public class BruteCollinearPoints {
                 }
 
         lineSegments = new LineSegment[points.length];
+
         double[] segmentSlopes = new double[points.length];
+
+        for (int i = 0; i < segmentSlopes.length; i++) {
+            segmentSlopes[i] = 0.0000000001;
+        }
+
         boolean segmentFoundAlready;
         double currentSlope;
 
@@ -27,7 +36,8 @@ public class BruteCollinearPoints {
             for (Point p2 : points) {
                 for (Point p3 : points) {
                     for (Point p4 : points) {
-                        if (p1 != p2 && p1 != p3 && p1 != p4) {
+                        if (p1 != p2 && p1 != p3 && p1 != p4
+                         && p2 != p3 && p2 != p4 && p3 != p4) {
 
                             if (p1.slopeTo(p4) == p2.slopeTo(p3)) {
 
@@ -37,8 +47,10 @@ public class BruteCollinearPoints {
                                 for (double slope : segmentSlopes)
                                     if (slope == currentSlope) segmentFoundAlready = true;
 
-
                                 if (!segmentFoundAlready) {
+
+                                    // determine the longest segment
+
                                     lineSegments[segCount] = new LineSegment(p1, p2);
                                     segmentSlopes[segCount] = currentSlope;
                                     segCount++;
@@ -52,6 +64,25 @@ public class BruteCollinearPoints {
             }
         }
 
+    }
+
+    private LineSegment findLongest(Point p1, Point p2, Point p3, Point p4) {
+
+
+
+    }
+
+    private double distance(Point a, Point b) {
+        String[] A = a.toString().split(", ");
+        String[] B = b.toString().split(", ");
+
+        int xA = Integer.parseInt(A[0].substring(1));
+        int yA = Integer.parseInt(A[1].substring(0, A[1].length()-1));
+
+        int xB = Integer.parseInt(B[0].substring(1));
+        int yB = Integer.parseInt(B[1].substring(0, A[1].length()-1));
+
+        return Math.sqrt( Math.pow( xA-xB, 2) + Math.pow( yA-yB, 2)  );
     }
 
     // the number of line segments
@@ -69,5 +100,35 @@ public class BruteCollinearPoints {
         }
 
         return returnSegs;
+    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 }
